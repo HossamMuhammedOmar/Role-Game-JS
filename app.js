d1a666ff7c91d3e9bee210db98331aeb597928dd
HossamMuhammedOmar <hossamomar9876@gmail.com>
2022-05-28 13:46:38 +0200

    destructring object data

--- app.js
+++ app.js
@@ -18,12 +18,13 @@ renderCharacter(hero);
 renderCharacter(monster);
 
 function renderCharacter(data) {
-  return (document.getElementById(`${data.id}`).innerHTML = `
+  const { id, name, avatar, health, score } = data;
+  document.getElementById(`${id}`).innerHTML = `
     <div class="character-card">
-    <h4 class="name">${data.name}</h4>
-    <img class="avatar" src="images/${data.avatar}" />
-    <p class="health">health: <b> ${data.health} </b></p>
-    <div class="dice-container"><div class="dice">${data.score}</div></div>
+    <h4 class="name">${name}</h4>
+    <img class="avatar" src="images/${avatar}" />
+    <p class="health">health: <b> ${health} </b></p>
+    <div class="dice-container"><div class="dice">${score}</div></div>
     </div>
-`);
+`;
 }
