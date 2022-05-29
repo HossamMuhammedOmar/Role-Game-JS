e7d324df8929ffd61c07eee805bfc4013809a6e2
HossamMuhammedOmar <hossamomar9876@gmail.com>
2022-05-29 18:24:56 +0200

    refactor js code

--- app.js
+++ app.js
@@ -1,30 +1,46 @@
+function getDiceRollArray(diceCount) {
+  return new Array(diceCount).fill(0).map(function () {
+    return Math.floor(Math.random() * 6) + 1;
+  });
+}
+
+function getDiceHtml(diceCount) {
+  return getDiceRollArray(diceCount)
+    .map(function (num) {
+      return `<div class="dice">${num}</div>`;
+    })
+    .join("");
+}
+
 const hero = {
-  id: "hero",
+  elementId: "hero",
   name: "Wizard",
-  avatar: "wizard.png",
+  avatar: "images/wizard.png",
   health: 60,
-  score: 6,
+  diceCount: 3,
 };
 
 const monster = {
-  id: "monster",
+  elementId: "monster",
   name: "Orc",
-  avatar: "orc.png",
+  avatar: "images/orc.png",
   health: 10,
-  score: 4,
+  diceCount: 1,
 };
+
+function renderCharacter(data) {
+  const { elementId, name, avatar, health, diceCount } = data;
+  const diceHtml = getDiceHtml(diceCount);
+
+  document.getElementById(elementId).innerHTML = `<div class="character-card">
+          <h4 class="name"> ${name} </h4>
+          <img class="avatar" src="${avatar}" />
+          <div class="health">health: <b> ${health} </b></div>
+          <div class="dice-container">    
+              ${diceHtml}
+          </div>
+      </div>`;
+}
 
 renderCharacter(hero);
 renderCharacter(monster);
-
-function renderCharacter(data) {
-  const { id, name, avatar, health, score } = data;
-  document.getElementById(`${id}`).innerHTML = `
-    <div class="character-card">
-    <h4 class="name">${name}</h4>
-    <img class="avatar" src="images/${avatar}" />
-    <div class="health">health: <b> ${health} </b></div>
-    <div class="dice-container"><div class="dice">${score}</div></div>
-    </div>
-`;
-}
