a714750809bd82197896f75ee6af85caa13636d1
HossamMuhammedOmar <hossamomar9876@gmail.com>
2022-06-02 06:45:29 +0200

    enhance return health bar

--- Character.js
+++ Character.js
@@ -49,15 +49,8 @@ function Character(data) {
 
   this.getHealthBarHtml = function () {
     const percent = getPercentage(this.health, this.maxHealth);
-    if (percent <= 25) {
-      return `<div class="health-bar-outer">
-      <div class="health-bar-inner danger" 
-          style="width: ${percent}%;">
-      </div>
-      </div>`;
-    }
-    return `<div class="health-bar-outer">
-    <div class="health-bar-inner ${percent} " 
+    return `<div class="health-bar-outer" >
+    <div class="health-bar-inner ${percent <= 25 ? "danger" : ""} " 
         style="width: ${percent}%;">
     </div>
     </div>`;
