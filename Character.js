7d323fb6214a1c0adb8666ec8733690adb4fbb7a
HossamMuhammedOmar <hossamomar9876@gmail.com>
2022-06-03 06:50:19 +0200

    convert constructor function to the class

--- Character.js
+++ Character.js
@@ -4,22 +4,24 @@ import {
   getPercentage,
 } from "/utils.js";
 
-function Character(data) {
-  Object.assign(this, data);
+class Character {
+  constructor(data) {
+    Object.assign(this, data);
 
-  this.diceHtml = getDicePlaceholderHtml(data.diceCount);
-  this.maxHealth = this.health;
+    this.diceHtml = getDicePlaceholderHtml(data.diceCount);
+    this.maxHealth = this.health;
+  }
 
-  this.setDiceHtml = function () {
+  setDiceHtml() {
     this.currentDiceScore = getDiceRollArray(this.diceCount);
     this.diceHtml = this.currentDiceScore
       .map((num) => {
         return `<div class="dice">${num}</div>`;
       })
       .join("");
-  };
+  }
 
-  this.getCharacterHtml = function () {
+  getCharacterHtml() {
     const { elementId, name, avatar, health, diceCount, diceHtml } = this;
     const healthBar = this.getHealthBarHtml();
     return `
@@ -32,9 +34,9 @@ function Character(data) {
                 </div>
                 ${healthBar}
             </div>`;
-  };
+  }
 
-  this.takeDamage = function (attackScoreArray) {
+  takeDamage(attackScoreArray) {
     const totalAttackScore = attackScoreArray.reduce(
       (total, currentElement) => {
         return total + currentElement;
@@ -45,16 +47,16 @@ function Character(data) {
       this.health = 0;
       this.dead = true;
     }
-  };
+  }
 
-  this.getHealthBarHtml = function () {
+  getHealthBarHtml() {
     const percent = getPercentage(this.health, this.maxHealth);
     return `<div class="health-bar-outer" >
     <div class="health-bar-inner ${percent <= 25 ? "danger" : ""} " 
         style="width: ${percent}%;">
     </div>
     </div>`;
-  };
+  }
 }
 
 export default Character;
